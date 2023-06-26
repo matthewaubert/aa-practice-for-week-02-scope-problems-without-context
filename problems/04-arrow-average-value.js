0106f70b1072b06670eb2ad9faf50dff59047922
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-06-26 19:09:57 -0400

    Solve problem 04

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,9 +10,19 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
+// function takes in an array of nums and returns average num
 const arrowAvgValue = array => {
-  // Your code here
+  // reduce to sum of nums in array
+  let sum = array.reduce((sum, num) => sum + num);
+
+  // return sum divided by number of elements in array
+  return sum / array.length;
 };
+
+
+// console.log(arrowAvgValue([10, 20])); // => 15
+// console.log(arrowAvgValue([2, 3, 7])); // => 4
+// console.log(arrowAvgValue([100, 60, 64])); // => 74.66666666666667
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
