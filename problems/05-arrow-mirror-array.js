13163b90a5d5cc6ca34a8737bf60e5ae44065dfe
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-06-27 09:28:30 -0400

    Solve problem 05

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -8,9 +8,17 @@ mirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
+// input: array
+// output: array with mirrored array concat'd
 const mirrorArray = (array) => {
-  // Your code here
+  // return array with mirrored array concat'd
+  return array.concat([...array].reverse());
 };
+
+console.log(mirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+console.log(mirrorArray(['a', 'b', 'c', 'd'])); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = mirrorArray;
